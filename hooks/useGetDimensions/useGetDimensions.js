8b9fb1d5b875b126b28eb7a5918e5bdcba0d8e53
AbraaoMorais <76604710+AbraaoMorais@users.noreply.github.com>
2022-02-13 16:52:22 -0300

    Add files via upload

---
+++ hooks/useGetDimensions/useGetDimensions.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect }  from 'react';
+
+export const  useGetDimensions = (myRef) => {
+    const hasWindow = typeof window !== 'undefined';
+    const getWindowDimensions = () => {
+        const width = hasWindow ? window.innerWidth : null;
+        const height = hasWindow ? window.innerHeight : null;
+        return {
+            width,
+            height,
+        };
+    }
+    const getComponentDimensions = () => ({
+        width: myRef.current.offsetWidth,
+        height: myRef.current.offsetHeight
+    });
+    const getDimensions = () => {
+        if (myRef) {
+            return getComponentDimensions()
+        }
+        return getWindowDimensions();
+    }
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+    useEffect(() => {
+        const handleResize = () => {
+            setDimensions(getDimensions());
+        };
+        if (hasWindow || myRef.current) {
+            setDimensions(getDimensions());
+        }
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [hasWindow]);
+    return dimensions;
+};
